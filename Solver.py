20a07eb4f49a147f6a86c2dcd228ce0c6779e873
Luka <lenixon@ucdavis.edu>
2023-03-14 18:16:57 -0700

    Completed Solver Function

--- Solver.py
+++ Solver.py
@@ -1,12 +1,13 @@
-import Board
+from Board import Board
 
-
+#Back Tracking algorithm
 def solve(board, rowIndex, columnIndex):
+    
     if rowIndex == 9:
         return True
     elif columnIndex == 9:
         return solve(board, rowIndex + 1, 0)
-    elif (board.getValue(rowIndex, columnIndex)):
+    elif (board.getValue(rowIndex, columnIndex)):     
         return solve(board, rowIndex, columnIndex+1)
     else:
         for value in range(1,10):
@@ -23,13 +24,26 @@ def solve(board, rowIndex, columnIndex):
 
         return False
     
-    
+def solveCaller(board):
+    if (solve(board,0,0)):
+        print("Board is solved:")
+        board.printBoard()
+    else:
+        print("The following board is unsolvable:")
+        board.printBoard()
 
 def main():
-    input = " "
+    input = "0 0 0 0 0 1 0 0 0 \
+             3 0 0 0 0 7 5 0 2\
+             4 0 0 0 5 0 1 9 0\
+             0 3 0 5 8 0 0 2 0\
+             0 0 8 0 0 0 7 0 0\
+             0 1 0 0 9 6 0 3 0\
+             0 8 3 0 7 0 0 0 6\
+             1 0 7 6 0 0 0 0 4\
+             0 0 0 2 0 0 0 0 0"
     board = Board(input)
-    
-    solve(board)
+    solveCaller(board)
 
 if __name__ == "__main__":
     main()
